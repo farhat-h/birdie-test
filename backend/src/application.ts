4b256f8a4a04690d25f1669fae011dd50c4e8df0
farhat-h <farhathaythem1@gmail.com>
2020-09-03 19:17:36 +0200

    add helmet middleware

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,10 +1,16 @@
 import * as express from "express";
+import * as helmet from 'helmet';
 import { apiController } from './controllers/api';
 import { pingController } from "./controllers/ping";
-
 const app = express();
 
+app.use(helmet())
 app.use(pingController);
 app.use('/api', apiController)
+
+app.get('*', (_, res) => {
+    /** Serve the react app */
+    return res.sendFile(`${__dirname}/client_build/index.html`)
+})
 
 export default app;
