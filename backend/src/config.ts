7cdfd3a1bce515bd63511a52e76315cda298fe3f
farhat-h <farhathaythem1@gmail.com>
2020-09-03 19:02:13 +0200

    run lint

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,10 +6,10 @@
 import * as dotEnv from 'dotenv'
 dotEnv.config({ path: `${__dirname}/.env` })
 export const DBCredentials = {
+    database: process.env.database,
     host: process.env.host,
+    password: process.env.password,
     port: process.env.port ? parseInt(process.env.port, 10) : 3306,
     user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
 
 }
