e486fc4f3b147ee658b343971b8b0187527bfb3e
farhat-h <farhathaythem1@gmail.com>
2020-09-03 18:58:11 +0200

    add react router

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -4,11 +4,15 @@ import registerServiceWorker from './registerServiceWorker';
 import store from '@App/store';
 import { Provider } from 'react-redux';
 import App from '@App/components/app/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
