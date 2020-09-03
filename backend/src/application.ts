29e00eb31fc0bd2d98fa18f8f41d7354f9f6d853
farhat-h <farhathaythem1@gmail.com>
2020-09-03 19:27:15 +0200

    add static route

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -5,12 +5,13 @@ import { pingController } from "./controllers/ping";
 const app = express();
 
 app.use(helmet())
+app.use(express.static(`${__dirname}/static`))
 app.use(pingController);
 app.use('/api', apiController)
 
 app.get('*', (_, res) => {
     /** Serve the react app */
-    return res.sendFile(`${__dirname}/client_build/index.html`)
+    return res.sendFile(`${__dirname}/static/index.html`)
 })
 
 export default app;
